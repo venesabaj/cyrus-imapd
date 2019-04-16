c07adaee4ffe9e1886611d224ffc010de1e3c08d
Ken Murchison <murch@fastmail.com>
2019-04-15 21:59:05 -0400

    Added conversations_guid_mbox_cmp(), mboxlist_lookup_by_guidrec(), and jmap_openmbox_by_guidrec() macros

--- imap/jmap_api.c
+++ imap/jmap_api.c
@@ -1183,9 +1183,7 @@ static int findblob_cb(const conv_guidrec_t *rec, void *rock)
     mbentry_t *mbentry = NULL;
     int r = 0;
 
-    r = (rec->version > CONV_GUIDREC_BYNAME_VERSION) ?
-        mboxlist_lookup_by_uniqueid(rec->mailbox, &mbentry, NULL) :
-        mboxlist_lookup(rec->mailbox, &mbentry, NULL);
+    r = mboxlist_lookup_by_guidrec(rec, &mbentry, NULL);
     if (r) {
         syslog(LOG_ERR, "jmap_findblob: no mbentry for %s", rec->mailbox);
         return r;
