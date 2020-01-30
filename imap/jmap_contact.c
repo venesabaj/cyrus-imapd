b16b4181d472d31889dfcb299e53db4f86480d2a
Ken Murchison <murch@fastmail.com>
2020-01-29 19:27:25 -0500

    jmap_contact.c: need to use mbentry when checking ACLs or opening mailbox when fetching blobId

--- imap/jmap_contact.c
+++ imap/jmap_contact.c
@@ -1792,6 +1792,7 @@ static int jmap_contact_getblob(jmap_req_t *req,
     struct carddav_data *cdata = NULL;
     struct mailbox *mailbox = NULL;
     struct vparse_card *vcard = NULL;
+    mbentry_t *mbentry = NULL;
     char *uid = NULL;
     modseq_t modseq;
     struct buf buf = BUF_INITIALIZER;
@@ -1816,7 +1817,9 @@ static int jmap_contact_getblob(jmap_req_t *req,
         res = HTTP_NOT_FOUND;
         goto done;
     }
-    if (!jmap_hasrights(req, cdata->dav.mailbox, JACL_READITEMS)) {
+
+    mbentry = jmap_mbentry_from_dav(req, &cdata->dav);
+    if (!mbentry || !jmap_hasrights_mbentry(req, mbentry, JACL_READITEMS)) {
         res = HTTP_NOT_FOUND;
         goto done;
     }
@@ -1828,7 +1831,7 @@ static int jmap_contact_getblob(jmap_req_t *req,
     }
 
     /* Open mailbox, we need it now */
-    if ((r = jmap_openmbox(req, cdata->dav.mailbox, &mailbox, 0))) {
+    if ((r = jmap_openmbox(req, mbentry->name, &mailbox, 0))) {
         req->txn->error.desc = error_message(r);
         res = HTTP_SERVER_ERROR;
         goto done;
@@ -1894,6 +1897,7 @@ done:
     if (vcard) vparse_free_card(vcard);
     if (mailbox) jmap_closembox(req, &mailbox);
     if (db) carddav_close(db);
+    mboxlist_entry_free(&mbentry);
     buf_free(&buf);
     free(uid);
     return res;
