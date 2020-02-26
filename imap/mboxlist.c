9abf3a8763977bec87d329de3b567c689dbed386
Ken Murchison <murch@fastmail.com>
2020-02-26 11:55:23 -0500

    Revert "mboxlist: use fastpath for uniqueid lookup for users too"
    This reverts commit 5f53aa78ced72fdf57573142de8c82571bfc3b4b.

--- imap/mboxlist.c
+++ imap/mboxlist.c
@@ -648,10 +648,10 @@ EXPORTED char *mboxlist_find_uniqueid(const char *uniqueid, const char *userid,
 
     init_internal();
 
-    if (userid && !have_runq)
+    if (userid)
         mboxlist_usermboxtree(userid, auth_state, _find_uniqueid, &rock, flags);
     else
-        mboxlist_foreach_uniqueid(uniqueid, _find_uniqueid, &rock, flags);
+        mboxlist_allmbox("", _find_uniqueid, &rock, flags);
 
     return rock.mboxname;
 }
@@ -3167,7 +3167,6 @@ struct _foreach_uniqueid_data {
     const char *uniqueid;
     mboxlist_cb *proc;
     void *rock;
-    int flags;
 };
 
 static int _foreach_uniqueid(const mbentry_t *mbentry, void *rock) {
@@ -3181,8 +3180,6 @@ static int _foreach_uniqueid(const mbentry_t *mbentry, void *rock) {
             copy.name = mbentry->name;
             return d->proc(&copy, d->rock);
         }
-        // if we're not doing tombstones, don't check the rest
-        if (!(d->flags & MBOXTREE_TOMBSTONES)) break;
     }
 
     return 0;
@@ -3191,7 +3188,7 @@ static int _foreach_uniqueid(const mbentry_t *mbentry, void *rock) {
 EXPORTED int mboxlist_foreach_uniqueid(const char *uniqueid, mboxlist_cb *proc,
                                        void *rock, int flags)
 {
-    struct _foreach_uniqueid_data crock = { uniqueid, proc, rock, flags };
+    struct _foreach_uniqueid_data crock = { uniqueid, proc, rock };
     struct allmb_rock mbrock = { NULL, _foreach_uniqueid, &crock, flags };
     int r = 0;
 
