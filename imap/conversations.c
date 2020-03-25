48962fe176c7a5f7c534a0132ecef766f7d32994
Ken Murchison <murch@fastmail.com>
2020-03-25 13:40:28 -0400

    conversations.c: use proper field name

--- imap/conversations.c
+++ imap/conversations.c
@@ -2628,8 +2628,8 @@ EXPORTED int conversation_update(struct conversations_state *state,
 
     for (folder = conv->folders; folder; folder = folder->next) {
         conv_status_t status;
-        const char *mboxname = strarray_nth(state->folder_names, folder->number);
-        int r = conversation_getstatus(state, mboxname, &status);
+        const char *mailbox = strarray_nth(state->folders, folder->number);
+        int r = conversation_getstatus(state, mailbox, &status);
         if (r) return r;
 
         int dirty = 0;
@@ -2680,7 +2680,7 @@ EXPORTED int conversation_update(struct conversations_state *state,
         }
 
         if (dirty) {
-            r = conversation_setstatus(state, mboxname, &status);
+            r = conversation_setstatus(state, mailbox, &status);
             if (r) return r;
         }
     }
