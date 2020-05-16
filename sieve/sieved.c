deae1384c94492d4f8172ea7c8e28e60d38621e4
Ken Murchison <murch@fastmail.com>
2020-05-16 09:55:05 -0400

    Sieve: deprecate "notify" in lieu of "enotify"

--- sieve/sieved.c
+++ sieve/sieved.c
@@ -1454,6 +1454,7 @@ static int generate_block(bytecode_input_t *bc, int pos, int end,
             break;
 
         case B_NOTIFY:
+            /* XXX  Translate this into enotify compatible */
             *requires |= SIEVE_CAPA_NOTIFY;
             generate_token("notify", indent, buf);
             generate_string(":method", cmd.u.n.method, buf);
